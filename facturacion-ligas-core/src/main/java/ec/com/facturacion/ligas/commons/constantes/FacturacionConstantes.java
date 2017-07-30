c322f71a8782fcfa2b0e3d671938917a31ca11a1
Amaru Iza <inkarri.programmer@gmail.com>
2017-07-29 21:18:22 -0500

    Nuevo estado

--- facturacion-ligas-core/src/main/java/ec/com/facturacion/ligas/commons/constantes/FacturacionConstantes.java
+++ facturacion-ligas-core/src/main/java/ec/com/facturacion/ligas/commons/constantes/FacturacionConstantes.java
@@ -15,5 +15,7 @@ public final class FacturacionConstantes {
 	public static final String ESTADO_ACTIVO = FacturacionResources.getString("ec.com.facturacio.ligas.estado.activo");
 	
 	public static final String ESTADO_INACTIVO = FacturacionResources.getString("ec.com.facturacio.ligas.estado.inactivo");
+	
+	public static final String ESTADO_FINALIZADO = "FIN";
 
 }
